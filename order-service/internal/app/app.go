588e9c7b2390dd3f7529c07763f9a32646a19c6c
Asliddin <sarvar@MacBook-Pro-Sarvar.local>
2026-03-30 23:38:11 +0300

    приводили app  в порядок

--- order-service/internal/app/app.go
+++ order-service/internal/app/app.go
@@ -6,14 +6,16 @@ import (
 
 	"github.com/asliddin03/go-grpc-order-service/order-service/internal/config"
 	postgresrepo "github.com/asliddin03/go-grpc-order-service/order-service/internal/repository/postgres"
+	"github.com/asliddin03/go-grpc-order-service/order-service/internal/service"
 	"github.com/asliddin03/go-grpc-order-service/order-service/internal/storage"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type App struct {
-	config    *config.Config
-	postgres  *pgxpool.Pool
-	orderRepo *postgresrepo.OrderRepository
+	config       *config.Config
+	postgres     *pgxpool.Pool
+	orderRepo    *postgresrepo.OrderRepository
+	orderService *service.OrderService
 }
 
 func New() *App {
@@ -36,10 +38,20 @@ func (a *App) Run() error {
 	defer postgresPool.Close()
 
 	a.postgres = postgresPool
-	a.orderRepo = postgresrepo.NewOrderRepository(postgresPool)
+
+	orderRepo := postgresrepo.NewOrderRepository(postgresPool)
+	a.orderRepo = orderRepo
+
+	orderService := service.NewOrderService(
+		nil,
+		nil,
+		orderRepo,
+	)
+	a.orderService = orderService
 
 	log.Println("order-service started")
 	log.Println("postgres connected")
 	log.Printf("order repository initialized: %T\n", a.orderRepo)
+	log.Printf("order service initialized: %T\n", a.orderService)
 	return nil
 }
